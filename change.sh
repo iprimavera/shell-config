6e8983981afc415a88181f349f6c941319395665
iprimavera <elsprimavera55@gmail.com>
2024-10-27 17:39:51 +0100

    1.1

--- change.sh
+++ change.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cat vim.txt > ~/.vimrc
 
 echo "** changed succesfully."
