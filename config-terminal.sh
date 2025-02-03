006dba4bd6740256ce7276fe53b366c65d346d17
iprimavera <elsprimavera55@gmail.com>
2025-02-03 09:27:50 +0100

    updated

--- config-terminal.sh
+++ config-terminal.sh
@@ -4,21 +4,34 @@
 PS3=$'\n'" * Elige una opcion: "
 
 
-select opcion in "añadir config de p10k (solo si tienes la fuente MesloLG)" "Salir"; do
+select opcion in "añadir config de p10k (solo si tienes la fuente MesloLG)" "instalar kitty" "config kitty" "Salir"; do
 	case $REPLY in
 
 ######################################################
 
 		1)
-			cat p10k.txt > ~/.p10k.zsh 
+			cat rc-files/p10k.txt > ~/.p10k.zsh 
 			#source ~/.zshrc
 			tput setaf 2; echo " ** archivo de configuracion de p10k cambiado."; tput sgr0
-			break
 		;;
 
 ######################################################
 
-		2) tput setaf 1; echo "Saliendo..."; tput sgr0; break 2;;
+		2)
+			curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+			tput setaf 2; echo " ** kitty instalado."; tput sgr0
+			tput setaf 3; echo " ** para hacer que kitty sea tu terminal por defecto cambia el atajo del teclado."; tput sgr0
+		;;
+
+######################################################
+
+		3)
+			cat rc-files/kitty.txt > ~/.config/kitty/kitty.conf
+		;;
+
+######################################################
+
+		4) tput setaf 1; echo "Saliendo..."; tput sgr0; break;;
 		*) tput setaf 1; echo "Opción inválida. Intenta de nuevo."; tput sgr0;;
 	esac
 done
