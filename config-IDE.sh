98a6823e9dee185bef3727037f20b46ac2836048
iprimavera <elsprimavera55@gmail.com>
2025-03-20 01:13:20 +0100

    Updated

--- config-IDE.sh
+++ config-IDE.sh
@@ -4,7 +4,7 @@
 PS3=$'\n'" * Elige una opcion: "
 
 
-select opcion in "install Plug de vim" "config vim" "install nvim" "config nvim" "Salir"; do
+select opcion in "install Plug de vim" "config vim" "install nvim" "Salir"; do
    	case $REPLY in
 
 ####################################################
@@ -28,19 +28,18 @@ select opcion in "install Plug de vim" "config vim" "install nvim" "config nvim"
 
 		3)
 			apt install neovim -y
-			mkdir -p ~/.config/nvim
 			tput setaf 2; echo " ** Neovim installed succesfully."; tput sgr0
 		;;
 
-		4)
-			cat rc-files/nvim/init.txt > ~/.config/nvim/init.lua
+		#4)
+		#	cat rc-files/nvim/init.txt > ~/.config/nvim/init.lua
 #			cat rc-files/nvim/lazy.txt > ~/.config/nvim/lua/lazy-cfg.lua
-			tput setaf 2; echo " ** Neovim configured succesfully."; tput sgr0
-		;;
+		#	tput setaf 2; echo " ** Neovim configured succesfully."; tput sgr0
+		#;;
 
 ######################################################
 
-		5) tput setaf 1; echo "Saliendo..."; tput sgr0; break;;
+		4) tput setaf 1; echo "Saliendo..."; tput sgr0; break;;
 		*) tput setaf 1; echo "Opción inválida. Intenta de nuevo."; tput sgr0;;
    	esac
 done
